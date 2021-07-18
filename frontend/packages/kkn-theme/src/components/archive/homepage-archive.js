19a7a974fedc2e63c415c1fd70e351d67142f8b8
RieGan <ganjarparikesit@gmail.com>
2021-07-18 18:47:38 +0700

    Rapiin homepage

--- frontend/packages/kkn-theme/src/components/archive/homepage-archive.js
+++ frontend/packages/kkn-theme/src/components/archive/homepage-archive.js
@@ -13,7 +13,7 @@ const HomepageArchive = ({ state, libraries }) => {
   const data = state.source.get(state.router.link);
   const [firstThreePosts, othersPosts] = splitPosts(state, data.items);
 
-  // Jika di home ("/")
+  // If in home ("/")
   var productData, product;
   if (state.router.link == "/") {
     productData = state.source.get(`/product`);
@@ -25,6 +25,40 @@ const HomepageArchive = ({ state, libraries }) => {
       <FeaturedPostSection
         data={firstThreePosts.map((post) => formatPostData(state, post))}
       />
+      {/* If in home ("/") */}
+      {state.router.link == "/" && (
+        <Box
+          py={{ base: "64px", md: "80px" }}
+          px={{ base: "24px", md: "40px" }}
+          width={{ base: "auto", lg: "80%" }}
+          maxWidth="1200px"
+          mx="auto"
+        >
+          <Heading
+            textTransform="uppercase"
+            textAlign="center"
+            fontSize={{ base: "4xl", md: "6xl" }}
+            color="accent.400"
+          >
+            Produk UMKM
+          </Heading>
+
+          <SimpleGrid
+            mt={{ base: "64px", md: "80px" }}
+            columns={{ base: 1, md: 4 }}
+            spacing="40px"
+          >
+            {product.map(({ type, id }) => {
+              const item = state.source[type][id];
+              return <ProductItem key={item.id} item={item} />;
+            })}
+          </SimpleGrid>
+
+          <PaginationButton mt="40px" link="/product">
+            More product
+          </PaginationButton>
+        </Box>
+      )}
       <Box
         py={{ base: "64px", md: "80px" }}
         px={{ base: "24px", md: "40px" }}
@@ -32,32 +66,6 @@ const HomepageArchive = ({ state, libraries }) => {
         maxWidth="1200px"
         mx="auto"
       >
-        {/* Jika di home ("/") */}
-        {state.router.link == "/" && (
-          <>
-            {/* Produk UMKM */}
-            <Heading
-              textTransform="uppercase"
-              textAlign="center"
-              fontSize={{ base: "4xl", md: "6xl" }}
-              color="accent.400"
-            >
-              Produk UMKM
-            </Heading>
-
-            <SimpleGrid
-              mt={{ base: "64px", md: "80px" }}
-              columns={{ base: 1, md: 4 }}
-              spacing="40px"
-            >
-              {product.map(({ type, id }) => {
-                const item = state.source[type][id];
-                return <ProductItem key={item.id} item={item} />;
-              })}
-            </SimpleGrid>
-          </>
-        )}
-
         {/* Posts */}
         <Heading
           textTransform="uppercase"
