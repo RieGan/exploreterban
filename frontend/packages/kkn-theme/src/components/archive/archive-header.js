4113fa93a9477b522d8040a1ea67765b19c697d3
RieGan <ganjarparikesit@gmail.com>
2021-07-26 22:06:17 +0700

    capitalize archive title and change bg color

--- frontend/packages/kkn-theme/src/components/archive/archive-header.js
+++ frontend/packages/kkn-theme/src/components/archive/archive-header.js
@@ -5,12 +5,12 @@ import { PatternBox, PatternBoxInner } from "../styles/pattern-box";
 const ArchiveHeader = ({ taxonomy, title, ...props }) => (
   <PatternBox pb="80px" mb="-80px" {...props}>
     <PatternBoxInner>
-      <Text textTransform="uppercase" color="#fff" fontWeight="bold">
+      <Text textTransform="capitalize" color="#fff" fontWeight="bold">
         {taxonomy}
       </Text>
       <Heading
         mt="8px"
-        textTransform="uppercase"
+        textTransform="capitalize"
         fontSize={{ base: "5xl", md: "6xl" }}
         color="accent.400"
       >
