6a3039e8e7f3ac538f66766c9c7585ae98db0c29
RieGan <ganjarparikesit@gmail.com>
2021-07-27 21:53:08 +0700

    new branching

--- frontend/packages/kkn-theme/src/components/archive/post-preview.js
+++ frontend/packages/kkn-theme/src/components/archive/post-preview.js
@@ -1,41 +1,74 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Center } from "@chakra-ui/react";
 import React from "react";
 import { PostImageWithOverlay } from "../featured-post/components";
 import Link from "../link";
 import PostTags from "../post/post-tags";
+import { styled } from "frontity";
+
+const Title = styled.p`
+font-family: Poppins;
+font-style: normal;
+font-weight: 600;
+font-size: 20px;
+line-height: 30px;
+color: #262626;
+`
+
+const PublishDate = styled.p`
+font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 14px;
+line-height: 21px;
+display: inline;
+`
 
 const PostPreview = ({ data, ...rest }) => {
-  const { title, excerpt, featured_media, link, tags } = data;
+  const { title, excerpt, featured_media, link, tags, publishDate } = data;
 
   return (
-    <Flex
-      direction="column"
-      position="relative"
-      bg="white"
-      as="article"
-      shadow="md"
-      {...rest}
-    >
-      {/* Use the frontity settings for featuredPost here */}
-      {featured_media && featured_media.src && (
-        <Link link={link}>
-          <PostImageWithOverlay {...featured_media} />
-        </Link>
-      )}
-
-      <Flex p="40px" flexGrow="1" direction="column">
-        <Heading fontSize="2xl" as="h4" textTransform="uppercase">
-          <Link link={link}>{title}</Link>
-        </Heading>
-        <Box
-          my="20px"
-          flex="1"
-          color="gray.700"
-          dangerouslySetInnerHTML={{ __html: excerpt }}
-        />
-        <PostTags color="black" justify="flex-start" tags={tags} />
+      <Flex
+          direction="column"
+          position="relative"
+          bg="white"
+          as="article"
+          shadow="md"
+          {...rest}
+      >
+        {/* Use the frontity settings for featuredPost here */}
+        {featured_media && featured_media.src && (
+            <Link link={link}>
+              <PostImageWithOverlay {...featured_media} />
+            </Link>
+        )}
+        <Flex p="40px" flexGrow="1" direction="column">
+            <PublishDate>
+              {publishDate + " | "}
+            </PublishDate>
+            <PostTags color="black" justify="flex-start" tags={tags} />
+          <Link link={link}>
+            <Title>
+              {title}
+            </Title>
+          </Link>
+          {/*<Heading fontSize="2xl" as="h4" textTransform="uppercase">*/}
+          {/*  <Link link={link}>{title}</Link>*/}
+          {/*</Heading>*/}
+          <Box
+              my="20px"
+              flex="1"
+              color="gray.700"
+              fontFamily="Poppins"
+              fontStyle="normal"
+              fontWeight="normal"
+              fontSize="16px"
+              lineHeight="26px"
+              overflow="hidden"
+              textOverflow="ellipsis"
+              dangerouslySetInnerHTML={{ __html: excerpt }}
+          />
+        </Flex>
       </Flex>
-    </Flex>
   );
 };
 
--- frontend/packages/kkn-theme/src/components/post/post-tags.js
+++ frontend/packages/kkn-theme/src/components/post/post-tags.js
@@ -2,41 +2,43 @@ import { Box, Flex } from "@chakra-ui/react";
 import React from "react";
 import Link from "../link";
 import { decode } from "frontity";
+import StyleControl from "../constant/style-control";
 
 export const PostTag = (props) => (
-  <Box
-    transition="background-color ease 0.25s"
-    px="5px"
-    border="2px solid"
-    borderColor="accent.400"
-    fontFamily="heading"
-    textTransform="uppercase"
-    fontWeight="medium"
-    display="inline-block"
-    _hover={{
-      bg: "accent.400",
-      color: props.color,
-    }}
-    {...props}
-  />
+    <Box
+        transition="background-color ease 0.25s"
+        px="5px"
+        border="2px solid"
+        borderRadius="5px"
+        borderColor={StyleControl.mainColor}
+        fontFamily="Poppins"
+        textTransform="capitalize"
+        fontWeight="medium"
+        display="inline-block"
+        _hover={{
+            bg: StyleControl.mainColor,
+            color: "#FFFFFF",
+        }}
+        {...props}
+    />
 );
 
 export const PostTags = ({ tags, limit = 69, color = "white", ...props }) => {
-  const limitTags =
-    tags.length > limit ? tags.filter((_, idx) => idx < limit) : tags;
+    const limitTags =
+        tags.length > limit ? tags.filter((_, idx) => idx < limit) : tags;
 
-  return (
-    <Flex flexWrap="wrap" mt="12px" {...props}>
-      {limitTags.map((tag) => (
-        <PostTag color={color} key={tag.id} mr="6px" mb="6px">
-          <Link
-            link={tag.link}
-            dangerouslySetInnerHTML={{ __html: decode(tag.name) }}
-          />
-        </PostTag>
-      ))}
-    </Flex>
-  );
+    return (
+        <Flex flexWrap="wrap" mt="12px" {...props}>
+            {limitTags.map((tag) => (
+                <PostTag color={color} key={tag.id} mr="6px" mb="6px">
+                    <Link
+                        link={tag.link}
+                        dangerouslySetInnerHTML={{ __html: decode(tag.name) }}
+                    />
+                </PostTag>
+            ))}
+        </Flex>
+    );
 };
 
 export default PostTags;
